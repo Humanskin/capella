32885c67569ea8d9ca158e9807a76933ec68bdbf
Humanskin <807492916@qq.com>
2020-02-24 14:51:22 +0800

    index.php第六行错误检测

--- capella/index.php
+++ capella/index.php
@@ -1,18 +1,19 @@
 <?php
 
-/**
- * Set the full path to the docroot
- */
-define('DOCROOT', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+echo __FILE__;
+// /**
+//  * Set the full path to the docroot
+//  */
+// define('DOCROOT', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 
-/**
- * Load the installation check
- */
-if (file_exists('install.php')) {
-    return include 'install.php';
-}
+// /**
+//  * Load the installation check
+//  */
+// if (file_exists('install.php')) {
+//     return include 'install.php';
+// }
 
-/**
- * Bootstrap the application
- */
-require DOCROOT . 'src/bootstrap.php';
+// /**
+//  * Bootstrap the application
+//  */
+// require DOCROOT . 'src/bootstrap.php';
